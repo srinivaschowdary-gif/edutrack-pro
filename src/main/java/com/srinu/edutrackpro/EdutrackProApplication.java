926a164e25870129eb781a9acb221d36d4f0f5ba
Srinivas Chowdary <kancharlasrinu11@gmail.com>
2026-06-05 09:34:49 +0530

    Initial Spring Boot Project setup

---
+++ src/main/java/com/srinu/edutrackpro/EdutrackProApplication.java
@@ -0,0 +1,13 @@
+package com.srinu.edutrackpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutrackProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdutrackProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/srinu/edutrackpro/EdutrackProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srinu.edutrackpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdutrackProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
