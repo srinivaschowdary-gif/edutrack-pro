b0ee1d69f35ef699590aa7d14617f0fbff37fd8e
Srinivas Chowdary <kancharlasrinu11@gmail.com>
2026-06-05 11:59:47 +0530

    Added validation annotations to Student entity

--- src/main/java/com/srinu/edutrackpro/entity/Student.java
+++ src/main/java/com/srinu/edutrackpro/entity/Student.java
@@ -1,5 +1,8 @@
 package com.srinu.edutrackpro.entity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Entity
@@ -12,27 +15,33 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Student code is required")
     @Column(name="student_code",nullable = false,unique = true)
     private String studentCode;
 
+    @NotBlank(message = "First name is required")
     @Column(name="first_name",nullable = false)
     private String firstName;
 
     @Column(name="last_name")
     private String lastName;
 
+    @NotBlank(message = "Invalid email formal")
     @Column(nullable = false,unique = true)
     private String email;
 
     @Column(name="phone_number")
     private String phoneNumber;
 
+    @NotBlank(message = "Department is required")
     @Column(nullable = false)
     private String department;
 
+    @NotBlank(message = "Semester is required")
     @Column(nullable = false)
     private String semester;
 
     @Enumerated(EnumType.STRING)
     private StudentStatus status;
+
 }
