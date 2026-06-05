803273c7b333aada60d3dbcffb1796656936c9be
Srinivas Chowdary <kancharlasrinu11@gmail.com>
2026-06-05 11:48:41 +0530

    Implemented  Student REST APIs

---
+++ src/main/java/com/srinu/edutrackpro/controller/StudentController.java
@@ -0,0 +1,39 @@
+package com.srinu.edutrackpro.controller;
+
+import com.srinu.edutrackpro.entity.Student;
+import com.srinu.edutrackpro.service.StudentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.PublicKey;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService){
+        this.studentService=studentService;
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student){
+        return studentService.saveStudent(student);
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable Long id){
+        return studentService.getStudentById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable Long id){
+        studentService.deleteStudent(id);
+    }
+}
